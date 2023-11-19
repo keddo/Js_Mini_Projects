8097c07975cba6e28f2396a09048a7a3cf1b59ad
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-19 16:29:38 +0300

    Update weather icon depending on the weather condition

--- Weather-App/js/index.js
+++ Weather-App/js/index.js
@@ -3,6 +3,9 @@ const API_URL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q
 
 const searchBox = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
+const weatherIcon = document.querySelector('.weather-icon');
+
+
 
 const checkWeather = async (city) => {
    const response = await fetch(API_URL + city + `&appid=${API_KEY}`);
@@ -13,9 +16,31 @@ const checkWeather = async (city) => {
    document.querySelector('.city').innerHTML = data.name;
    document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
    document.querySelector('.wind').innerHTML = Math.round(data.wind.speed) + " Km/h";
+   setWeatherIcon(data);
 
 }
 
+const setWeatherIcon = (data) => {
+    switch(data.weather[0].main) {
+        case "Clouds":
+            weatherIcon.src = 'images/clouds.png';
+            break;
+        case "Clear":
+            weatherIcon.src = 'images/clear.png';
+            break;
+        case "Rain":
+            weatherIcon.src = 'images/rain.png';
+            break;
+        case "Drizzle":
+            weatherIcon.src = 'images/drizzle.png';
+            break;
+        case "Mist":
+            weatherIcon.src = 'images/mist.png';
+            break;
+        default:
+            weatherIcon.src = 'images/clear.png';
+    }
+}
 searchBtn.addEventListener('click', () => {
     checkWeather(searchBox.value);
 });
