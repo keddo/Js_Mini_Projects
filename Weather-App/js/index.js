92676914293b45acca549704eb6e1e404dc33aa2
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-19 16:02:45 +0300

    Populate data on button clicked?

--- Weather-App/js/index.js
+++ Weather-App/js/index.js
@@ -1,8 +1,11 @@
 const API_KEY = "9a0fc904a12b0b9bb9e138635152f4c5";
-const API_URL = "https://api.openweathermap.org/data/2.5/weather?q=Addis Ababa&units=metric";
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
-const checkWeather = async () => {
-   const response = await fetch(API_URL + `&appid=${API_KEY}`);
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+
+const checkWeather = async (city) => {
+   const response = await fetch(API_URL + city + `&appid=${API_KEY}`);
    const data = await response.json();
    console.log(data);
 
@@ -13,4 +16,6 @@ const checkWeather = async () => {
 
 }
 
-checkWeather();
+searchBtn.addEventListener('click', () => {
+    checkWeather(searchBox.value);
+});
